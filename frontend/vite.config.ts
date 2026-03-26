68afbf6f8ce4c1f71481937bc50a4016e8d0c1c6
Rajat Dubey <157128257+Rajat-Dub3y@users.noreply.github.com>
2026-03-27 00:41:32 +0530

    Remove changeOrigin from Vite proxy configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://hollaapp.onrender.com/",
-        changeOrigin: true,
         secure: false,
       },
     }
